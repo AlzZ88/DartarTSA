8d3ac3eec80797464fe8faf026ecfca2880f74f5
AlzZ <asanchez2017@udec.cl>
2023-12-18 07:44:33 -0300

    18/12/2023

--- scenes/scenario/Scenario.gd
+++ scenes/scenario/Scenario.gd
@@ -1,23 +1,36 @@
 extends Node2D
 class_name Scenario
-
-var A_zones=[
-	preload("res://scenes/scenario/zone/Zone.tscn")
-]
-
-
 var scenario_name = ""
+var biome=""
 
 var camps = []
-func _ready():
-	instanciarHabitacion(Vector2(0,0))
 
-
-func _init(name_scenario):
+func _init_(name_scenario,_biome):
 	scenario_name = name_scenario
+	biome=_biome
+	spawn_room(Vector2(-48*16,-53*16),biome,"A")
+	spawn_room(Vector2(16*16,-53*16),biome,"I")
+	spawn_room(Vector2(48*16,-53*16),biome,"K")
+	spawn_room(Vector2(-48*16,-21*16),biome,"B")
+	spawn_room(Vector2(-48*16,11*16),biome,"C")
+	spawn_room(Vector2(-16*16,-21*16),biome,"H")
+	spawn_room(Vector2(16*16,-21*16),biome,"J")
+	spawn_room(Vector2(-16*16,11*16),biome,"G")
+	spawn_room(Vector2(-48*16,43*16),biome,"D")
+	spawn_room(Vector2(16*16,43*16),biome,"E")
+	spawn_room(Vector2(48*16,43*16),biome,"F")
 	
-func instanciarHabitacion(coordenadas: Vector2):
-	print(coordenadas)
-	var habitacion = preload("res://scenes/scenario/zone/Zone.tscn")
-	var instance=habitacion.instantiate()
-	add_child(instance)
+	
+	
+func spawn_room(Room_cord: Vector2,biome,zone):
+
+	var Zones=["a","b","c"]
+	var rng = RandomNumberGenerator.new()
+	var variation=Zones[rng.randi_range(0,Zones.size()-1)]
+	var path="res://scenes/scenario/"+biome+"/"+zone+"/"+zone+"_"+variation+".tscn"
+	print(path)
+	var Room = load(path)
+	#print("loading "+biome+" "+zone+" Zone "+variation+" variation.")
+	var Room_instance=Room.instantiate()
+	Room_instance.position=Room_cord
+	add_child(Room_instance)
--- scripts/Campaign/Mission.gd
+++ scripts/Campaign/Mission.gd
@@ -6,9 +6,31 @@ var difficulty = 0
 var rewards = []
 var scenario = null
 
-func _init(title_mission, desc_mission, difficulty_mission, rewards_mission,scenario_mission):
-	title = title_mission
-	description = desc_mission
-	difficulty = difficulty_mission
-	rewards = rewards_mission
-	scenario = scenario_mission
+func add_title(new_title : String) -> Mission:
+	title = new_title
+	return self
+
+func add_description(new_description : String) -> Mission:
+	description = new_description
+	return self
+
+func add_goals(new_goals : Array) -> Mission:
+	goals = new_goals
+	return self
+
+func add_difficulty(new_difficulty : int) -> Mission:
+	difficulty = new_difficulty
+	return self
+
+func add_rewards(new_rewards : Array) -> Mission:
+	rewards = new_rewards
+	return self
+
+func add_scenario(scenario_name,biome,main) -> Mission:
+	var scenario_instance =preload("res://scenes/scenario/scenario.tscn").instantiate()
+	scenario_instance.position=Vector2(0,0)
+	scenario_instance._init_(scenario_name,biome)
+	scenario=scenario_instance
+	main.add_child(scenario_instance)
+	return self
+	
